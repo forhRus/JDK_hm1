2e23af9ff242e6afe0d70b8602cb331813f1e6a2
SergSH <shkaev.ss@mail.ru>
2023-10-02 20:45:28 +0300

    Вспомнил, что у меня есть гит, когда доделал проект

--- src/ClientGUI.java
+++ src/ClientGUI.java
@@ -2,6 +2,10 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 public class ClientGUI extends JFrame {
     private final int WINDOW_WIDTH = 500;
@@ -13,16 +17,21 @@ public class ClientGUI extends JFrame {
     private final String BTN_SEND = "send";
     private final String JTF_IP = "127.0.0.1";
     private final String JTF_HOST = "Ivan Ivanovich";
-    private final String JTF_SOCET = "8189";
+    private final String JTF_SOCKET = "8189";
     private final String JTF_PWD = "12345678";
-    JTextArea textArea;
-    JTextField jtfIp, jtfSocet, jtfHost, jtfSend;
-    JPasswordField jtfPwd;
-    JButton btnLogin, btnSend;
-    StringBuilder sbTextArea;
+    private final String CONNECTED = "You are connected to server";
+
+    private JTextArea jtaChat;
+    private JTextField jtfIp, jtfSocket, jtfHost, jtfSend;
+    private JPasswordField jtfPwd;
+    private JButton btnLogin, btnSend;
+    private ServerWindow server;
+    private String host, ip, socket;
+    private boolean isLogined;
 
 
-    public ClientGUI() throws HeadlessException {
+    public ClientGUI(ServerWindow serverWindow) throws HeadlessException {
+        server = serverWindow;
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
         setLocation(WINDOW_POS_X, WINDOW_POS_Y);
@@ -30,30 +39,53 @@ public class ClientGUI extends JFrame {
         setTitle(TITLE);
         setResizable(false);
 
-        textArea = new JTextArea();
-        textArea.setEditable(false); //запретить редактировать с клавиатуры
-
+        jtaChat = new JTextArea();
+        jtaChat.setEditable(false); //запретить редактировать с клавиатуры
 
         add(createTopPanel(), BorderLayout.NORTH);
-        add(textArea);
+        add(jtaChat);
         add(createBotPanel(), BorderLayout.SOUTH);
-        
+
         setVisible(true); // делаем видимым
     }
 
-    private Component createBotPanel(){
+    private Component createBotPanel() {
         JPanel panel = new JPanel(new BorderLayout());
+
         JTextField jtfSend = new JTextField();
         btnSend = new JButton(BTN_SEND);
-        btnSend.setEnabled(false);
+        btnSend.setEnabled(false); // кнопка неактивна
+
         btnSend.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String send = jtfSend.getText();
-                jtfSend.setText("");
-                textArea.append(send+"\n");
+                if (server.isServerState()) { // проверка активности сервера
+                    String msg = jtfSend.getText();
+                    if (msg.length() == 0) {
+                        return;
+                    }
+                    msg = addTimeMsg(jtfSend.getText()); // добавление времени к
+                    jtfSend.setText("");  // удаление содержимого в текстовом поле
+                    server.writeChat(host, msg);
+                    jtaChat.append(msg); // отправка в чат сообщение из текстового поля
+                } else {
+                    btnSend.setEnabled(false); // отключение кнопки
+                    jtaChat.append(addTimeMsg(server.getSERVER_INACTIVE()));
+                }
             }
         });
+
+        // отправка по интеру
+        jtfSend.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    btnSend.doClick(); // жмём send там все проверки
+                }
+            }
+        });
+
+        // панель с двумя компонентами
         panel.add(jtfSend);
         panel.add(btnSend, BorderLayout.EAST);
 
@@ -62,16 +94,17 @@ public class ClientGUI extends JFrame {
 
     /**
      * Создаёт верхнюю панель
+     *
      * @return
      */
     private Component createTopPanel() {
         JPanel panel = new JPanel(new GridLayout(2, 3));
         JTextField jtfIp = new JTextField();
         jtfIp.setText(JTF_IP);
-        JTextField jtfSocet = new JTextField();
-        jtfSocet.setText(JTF_SOCET);
+        JTextField jtfSocket = new JTextField();
+        jtfSocket.setText(JTF_SOCKET);
         panel.add(jtfIp);
-        panel.add(jtfSocet);
+        panel.add(jtfSocket);
 
         // вставка пустой ячейки
         panel.add(new JPanel(new GridLayout()));
@@ -80,11 +113,26 @@ public class ClientGUI extends JFrame {
         jtfHost.setText(JTF_HOST);
         JTextField jtfPwd = new JPasswordField();
         jtfPwd.setText(JTF_PWD);
+        isLogined = false;
         btnLogin = new JButton(BTN_LOGIN);
         btnLogin.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                btnSend.setEnabled(true);
+                if (server.isServerState()) {
+                    if (!isLogined) { // если залогинились, не делать этого повторно.
+                        isLogined = true;
+                        host = jtfHost.getText();
+                        ip = jtfIp.getText();
+                        jtaChat.setText("");
+                        jtaChat.append(server.loadChat(host)); // выгружаем чат с сервера
+                        server.connectToServer(ClientGUI.this); // отдаём серверу клиента для логирования
+                        jtaChat.append(addTimeMsg(CONNECTED));
+                        btnSend.setEnabled(true); // активация кнопки отправить
+                    }
+                } else {
+                    isLogined = false;
+                    jtaChat.append(addTimeMsg(server.getSERVER_INACTIVE()));
+                }
             }
         });
         panel.add(jtfHost);
@@ -94,5 +142,20 @@ public class ClientGUI extends JFrame {
         return panel;
     }
 
+    // геттеры для сервера
+    public String getIp() {
+        return ip;
+    }
 
+    public String getHost() {
+        return host;
+    }
+
+    // время для сообщений
+    private String addTimeMsg(String s) {
+        LocalTime time = LocalTime.now();
+        String t = time.format(DateTimeFormatter.ofPattern("HH:mm:ss"));
+        String msg = String.format("%s: %s\n", t, s);
+        return msg;
+    }
 }
--- src/ServerWindow.java
+++ src/ServerWindow.java
@@ -2,23 +2,29 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.*;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 public class ServerWindow extends JFrame {
     private final int WINDOW_WIDTH = 500;
     private final int WINDOW_HEIGHT = 400;
     private final int WINDOW_POS_X = 800;
     private final int WINDOW_POS_Y = 300;
-    private  final String TITLE = "Chat server";
-    private  final String BTN_START = "Start";
-    private  final String BTN_STOP = "Stop";
-    private  final String SERVER_STARTED = "Server started";
-    private  final String SERVER_STOPPED = "Server stopped";
-    private  final String SERVER_ISRUNNING = "Server is already running";
+    private final String TITLE = "Chat server";
+    private final String BTN_START = "Start";
+    private final String BTN_STOP = "Stop";
+    private final String SERVER_STARTED = "Server is started";
+    private final String SERVER_STOPPED = "Server is stopped";
+    private final String SERVER_INACTIVE = "Server is inactive";
+    private final String SERVER_ISRUNNING = "Server is already running";
+    private final String PATH_HOSTS_DIR = "./src/hosts";
+    private final String CHAT_FILE = "Chat.txt";
+    private final String LOGS_FILE = "logs.txt";
 
     private boolean serverState;
-    JTextArea textArea;
-    StringBuilder sbTextArea;
-
+    JTextArea jtaLogServer;
 
     public ServerWindow() throws HeadlessException {
         setDefaultCloseOperation(EXIT_ON_CLOSE); //завершать программу при закрытие окна сервера
@@ -29,38 +35,40 @@ public class ServerWindow extends JFrame {
         setResizable(false); //Запретить менять размеры окна
         serverState = false;
 
-        textArea = new JTextArea();
-        textArea.setEditable(false); //запретить редактировать с клавиатуры
-        add(textArea); // текстовое поле
+        jtaLogServer = new JTextArea();
+        jtaLogServer.setEditable(false); //запретить редактировать с клавиатуры
+        add(jtaLogServer); // текстовое поле
 
         add(createStartStopPanel(), BorderLayout.SOUTH); // панель с кнопками start и stop
         setVisible(true); // делаем видимым
     }
 
-
-
-    private Component createStartStopPanel(){
+    /**
+     * Создание южной панели
+     *
+     * @return
+     */
+    private Component createStartStopPanel() {
         JPanel panel = new JPanel(new GridLayout(1, 2));
         JButton btnStart = new JButton(BTN_START);
         btnStart.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if(serverState){
-                    textArea.append(SERVER_ISRUNNING);
-                } else{
-                    serverState = true;
-                    textArea.append(SERVER_STARTED);
+                if (serverState) {
+                    sendTo(SERVER_ISRUNNING); // сервер уже включён
+                } else {
+                    serverState = true; // включить сервер
+                    sendTo(SERVER_STARTED);
                 }
-
             }
         });
         JButton btnStop = new JButton(BTN_STOP);
         btnStop.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if(serverState){
-                    serverState = false;
-                    textArea.append(SERVER_STOPPED);
+                if (serverState) {
+                    serverState = false; // отключить сервер
+                    sendTo(SERVER_STOPPED);
                 }
             }
         });
@@ -68,4 +76,125 @@ public class ServerWindow extends JFrame {
         panel.add(btnStop);
         return panel;
     }
+
+    /**
+     * Проверка активности сервера
+     *
+     * @return
+     */
+    public boolean isServerState() {
+        return serverState;
+    }
+
+    /**
+     * Получает из клиента host и ip формирует строку для логирования.
+     *
+     * @param client
+     * @return
+     */
+    public boolean connectToServer(ClientGUI client) {
+        if (serverState) {
+            String s = String.format("host:%s, ip:%s is connected",
+                    client.getHost(), client.getIp());
+            String msg = addTimeMsg(s);
+            writeLog(msg, client.getHost());
+            jtaLogServer.append(msg);
+        }
+        return serverState;
+    }
+
+    /**
+     * Геттер неактивного статуса для невозможности залогинится при отключенном сервере
+     *
+     * @return
+     */
+    public String getSERVER_INACTIVE() {
+        return SERVER_INACTIVE;
+    }
+
+    /**
+     * Запись в лог файл клиента о его активности
+     *
+     * @param msg
+     * @param host
+     */
+    private void writeLog(String msg, String host) {
+        File logs = new File(checkHostDir(host), LOGS_FILE);
+        try (FileWriter fw = new FileWriter(logs, true);) {
+            fw.write(msg);
+        } catch (IOException e) {
+            new RuntimeException(e);
+        }
+    }
+
+    /**
+     * вывод в лог сервера сообщения с добавлением времени
+     *
+     * @param msg
+     */
+    private void sendTo(String msg) {
+        jtaLogServer.append(addTimeMsg(msg));
+    }
+
+    /**
+     * Добавить время к строке
+     *
+     * @param s
+     * @return
+     */
+    private String addTimeMsg(String s) {
+        LocalTime time = LocalTime.now();
+        String t = time.format(DateTimeFormatter.ofPattern("HH:mm:ss"));
+        String msg = String.format("%s: %s\n", t, s);
+        return msg;
+    }
+
+    /**
+     * Проверяет наличие дириктории у хоста и создаёт её, если она отстустствует
+     *
+     * @param host имя директории
+     * @return директория хоста
+     */
+    private File checkHostDir(String host) {
+        File hostDir = new File(String.format("%s/%s", PATH_HOSTS_DIR, host));
+        if (!hostDir.exists()) {
+            hostDir.mkdirs();
+        }
+        return hostDir;
+    }
+
+    /**
+     * Записывает чат в файл. Если дириктории у хоста нет, то создаёт её.
+     *
+     * @param host
+     * @param msg
+     */
+    public void writeChat(String host, String msg) {
+        File chat = new File(checkHostDir(host), CHAT_FILE);
+        try (FileWriter fw = new FileWriter(chat, true);) {
+            fw.write(msg);
+        } catch (IOException e) {
+            new RuntimeException(e);
+        }
+    }
+
+    public String loadChat(String host) {
+        File hostDir = checkHostDir(host);
+        File chat = new File(hostDir, CHAT_FILE);
+        if (chat.exists()) {
+            try (BufferedReader br = new BufferedReader(new FileReader(chat))) {
+                StringBuilder sb = new StringBuilder();
+                String s;
+                while ((s = br.readLine()) != null) {
+                    sb.append(s);
+                    sb.append("\n");
+                }
+                return sb.toString();
+            } catch (IOException e) {
+                new RuntimeException(e);
+            }
+        }
+        return "";
+    }
+
 }
